da4863d868b8d6b6d6ce7a423e98978a7c44a8b5
vetasbike <vetasbike@mail.ru>
2023-04-07 15:50:35 +0300

    lesson_4 Done

--- Blog/models.py
+++ Blog/models.py
@@ -8,9 +8,9 @@ class User(db.Model, UserMixin):
 
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(255), unique=True)
+    name = db.Column(db.String(255))
     password = db.Column(db.String(255))
 
     def __init__(self, email, password):
         self.email = email
-        self.password =
-        
+        self.password = password
